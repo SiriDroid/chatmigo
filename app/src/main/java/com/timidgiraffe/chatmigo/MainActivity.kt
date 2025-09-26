eb15ec8a2d15d65e0fb33ffb4b3aa009c7f7af9b
Siri <siri@Siris-Air.attlocal.net>
2025-09-26 13:02:41 -0500

    initial commit

---
+++ app/src/main/java/com/timidgiraffe/chatmigo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.timidgiraffe.chatmigo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.timidgiraffe.chatmigo.ui.theme.ChatmigoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChatmigoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    BakingScreen()
+                }
+            }
+        }
+    }
+}
