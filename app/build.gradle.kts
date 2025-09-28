92641e55d6a395ea63349c300ce5419b02075166
Siri <siri@Siris-Air.attlocal.net>
2025-09-28 18:42:27 -0500

    new chat screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.timidgiraffe.chatmigo"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.timidgiraffe.chatmigo"
